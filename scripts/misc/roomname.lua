484af235d8b5c857a9798895d5d62914bf598ba7
GabeHasWon <gabriel@guevarameyer.com>
2025-09-14 19:07:57 -0500

    update 1.26 I think

--- scripts/misc/roomname.lua
+++ scripts/misc/roomname.lua
@@ -1,7 +1,7 @@
 WOMBPLUS:AddCallback(ModCallbacks.MC_POST_RENDER, function()
 	local room = Game():GetLevel():GetCurrentRoomDesc().Data.Name
 
-	if string.match(room, "^ScarredWomb+ -") then
+	if string.match(room, "^ScarredWomb+ -") and not game:GetSeeds():HasSeedEffect(SeedEffect.SEED_NO_HUD) then
 		local x = Isaac.GetScreenWidth() / 2 - Isaac.GetTextWidth(room) / 2
 		Isaac.RenderText(room, x, Isaac.GetScreenHeight() - 16, 255, 255, 255, 0.8)
 	end
